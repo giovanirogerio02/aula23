554973409dbb6221ec3b37504d2195f26bd3fd56
giovanirogerio02 <Geovannirogeriogr@gmail.com>
2024-07-30 17:32:44 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,68 @@
+
+<?php
+
+//Crie uma variável com uma frase, e aplique todas as funções nativas
+//mostradas nos slides.
+
+
+echo $"A natureza é perfeita";
+
+strtolower($onça pintada):
+
+Strtoupper($papagaios):
+
+ucfirst($Leões):
+
+strlen($tatus):
+
+str_replace($cobras): 
+
+substr($gorilas):
+
+$texto="O Brasil é um país patriota";
+echo $Viva os brasileiros . "\n";
+echo strtolower($salve a nossa pátria);
+
+$texto="A natureza é exiberante";
+echo $texto . "\n";
+echo strtoupper($encantadora);
+
+
+$"O Brasil"=“é um país patriota";
+echo $"Viva, os brasileiros" . "\n";
+echo ucfirst($"Salve a nação");
+
+$"A natureza" =“com seus encantos e exiberancias";
+echo $A preservação . "\n";
+echo strlen($"O home precisa preservar a natureza");
+
+$texto = "O Python é uma linguagem de programação muito popular.";
+// Palavra a ser substituída
+$procura = "Python";
+// Palavra de substituição
+$substitui = "PHP";
+// Usando str_replace
+$novo_texto = str_replace($procura, $substitui, $texto);
+echo $novo_texto;
+
+
+// String original
+$texto = “Monique";
+
+// Usando substr para pegar uma parte da string
+$subtexto = substr($texto, 0, 5);
+
+echo $subtexto;
+
+
+
+
+
+
+
+
+
+
+
+?>
+
